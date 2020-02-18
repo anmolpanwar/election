d16d494267adedfbbcbb080e5208b4a4df615c4b
Anmol Panwar <anmolpanwar8@gmail.com>
2020-02-18 19:47:47 +0530

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,6 +1,6 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
+  // for more about customizing your Truffle!
   networks: {
     development: {
       host: "127.0.0.1",
